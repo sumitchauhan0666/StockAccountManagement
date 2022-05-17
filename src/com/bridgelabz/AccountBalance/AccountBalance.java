19a103f670e28cb33c0f5f108e0242e374731e08
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-17 23:55:44 +0530

    Day 11 and 12 Programme

---
+++ src/com/bridgelabz/AccountBalance/AccountBalance.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.AccountBalance;
+
+public class AccountBalance {
+    private double acc_bal;
+
+    public double getAcc_bal(){
+        return acc_bal;
+    }
+    public void setAcc_bal(double acc_bal){
+        this.acc_bal = acc_bal;
+    }
+}
